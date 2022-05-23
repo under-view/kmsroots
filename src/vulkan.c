d62a79ece6845255e6c66a106a845d4279be3cd4
Vincent Davis Jr <vince@underview.tech>
2022-05-22 20:09:50 -0500

    vulkan: update VkResult error messages
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- src/vulkan.c
+++ src/vulkan.c
@@ -32,19 +32,6 @@ static const char *vkres_msg(int err) {
       return "a requested format is not supported on this device";
     case VK_ERROR_FRAGMENTED_POOL:
       return "a pool allocation has failed due to fragmentation of the pool's memory";
-    case VK_ERROR_SURFACE_LOST_KHR:
-      return "a surface is no longer available";
-    case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
-      return "the requested window is already in use by Vulkan or another API in a manner which " \
-             "prevents it from being used again";
-    case VK_ERROR_OUT_OF_DATE_KHR:
-      return "a surface has changed in such a way that it is no longer compatible with the swapchain, " \
-             "any further presentation requests using the swapchain will fail";
-    case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
-      return "The display used by a swapchain does not use the same presentable image layout, or is " \
-             "incompatible in a way that prevents sharing an image";
-    case VK_ERROR_INVALID_SHADER_NV:
-      return "one or more shaders failed to compile or link";
     case VK_ERROR_OUT_OF_POOL_MEMORY:
       return "a pool memory allocation has failed";
     case VK_ERROR_INVALID_EXTERNAL_HANDLE:
@@ -53,11 +40,48 @@ static const char *vkres_msg(int err) {
       return "a descriptor pool creation has failed due to fragmentation";
     case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS:
       return "a buffer creation or memory allocation failed because the requested address is not available";
+    case VK_PIPELINE_COMPILE_REQUIRED:
+      return "A requested pipeline creation would have required compilation, but the application requested" \
+             "compilation to not be performed.";
+    case VK_ERROR_SURFACE_LOST_KHR:
+      return "a surface is no longer available";
+    case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
+      return "the requested window is already in use by Vulkan or another API in a manner which " \
+             "prevents it from being used again";
+    case VK_SUBOPTIMAL_KHR:
+      return "A swapchain no longer matches the surface properties exactly, but can still be used to present" \
+             "to the surface successfully.";
+    case VK_ERROR_OUT_OF_DATE_KHR:
+      return "a surface has changed in such a way that it is no longer compatible with the swapchain, " \
+             "any further presentation requests using the swapchain will fail";
+    case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
+      return "The display used by a swapchain does not use the same presentable image layout, or is " \
+             "incompatible in a way that prevents sharing an image";
+    case VK_ERROR_VALIDATION_FAILED_EXT:
+      return "VK_ERROR_VALIDATION_FAILED_EXT";
+    case VK_ERROR_INVALID_SHADER_NV:
+      return "one or more shaders failed to compile or link";
+    case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT:
+      return "VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT";
+    case VK_ERROR_NOT_PERMITTED_KHR:
+      return "VK_ERROR_NOT_PERMITTED_KHR";
     case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT:
       return "an operation on a swapchain created with VK_FULL_SCREEN_EXCLUSIVE_APPLICATION_CONTROLLED_EXT failed as " \
              "it did not have exlusive full-screen access";
+    case VK_THREAD_IDLE_KHR:
+      return "A deferred operation is not complete but there is currently no work for this thread to do at the time of this call.";
+    case VK_THREAD_DONE_KHR:
+      return "A deferred operation is not complete but there is no work remaining to assign to additional threads.";
+    case VK_OPERATION_DEFERRED_KHR:
+      return "A deferred operation was requested and at least some of the work was deferred.";
+    case VK_OPERATION_NOT_DEFERRED_KHR:
+      return "A deferred operation was requested and no operations were deferred.";
+    case VK_ERROR_COMPRESSION_EXHAUSTED_EXT:
+      return "An image creation failed because internal resources required for compression are exhausted." \
+             "This must only be returned when fixed-rate compression is requested.";
   }
 
+  // VK_ERROR_UNKNOWN
   return "An unknown error has occurred. Either the application has provided invalid input, or an implementation failure has occurred";
 }
 
