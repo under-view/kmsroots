fdea6cdce3e6ed36e4df92e36a0f4f6a6726b5cc
Vincent Davis Jr <vince@underview.tech>
2023-01-10 20:00:08 -0600

    wserver: add required arg to wlr_backend_autocreate
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- src/wserver.c
+++ src/wserver.c
@@ -26,7 +26,7 @@ struct uvr_ws_core uvr_ws_core_create(struct uvr_ws_core_create_info *uvrws)
     goto exit_ws_core;
   }
 
-  core.wlrBackend = wlr_backend_autocreate(core.wlDisplay);
+  core.wlrBackend = wlr_backend_autocreate(core.wlDisplay, NULL);
   if (!core.wlrBackend) {
     uvr_utils_log(UVR_DANGER, "[x] wlr_backend_autocreate: Failed to create backend {DRM/KMS, Wayland, X, Headless}");
     goto exit_ws_core_wl_display_destory;
