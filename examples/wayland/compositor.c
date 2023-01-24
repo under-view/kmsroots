c107a723d1c8c4f7eae7e1b97fc655d2f122eb92
Vincent Davis Jr <vince@underview.tech>
2023-01-24 11:18:13 -0600

    examples: impl signal handler in compositor.c
    Used to distroy terminate compositor and release
    addresses. Known issue segfault at exit.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- examples/wayland/compositor.c
+++ examples/wayland/compositor.c
@@ -1,10 +1,37 @@
-#include "wserver.h"
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+
+#include "wserver.h"
+
+static struct uvr_ws_destroy ws;
+
+
+static void run_stop(int UNUSED _)
+{
+  uvr_ws_destroy(&ws);
+}
+
 
 int main(void)
 {
+  /* Install signal handlers. */
+  if (signal(SIGINT, run_stop) == SIG_ERR) {
+    fprintf(stderr, "[x] signal: Error while installing SIGINT signal handler.\n");
+    return 1;
+  }
+
+  if (signal(SIGABRT, run_stop) == SIG_ERR) {
+    fprintf(stderr, "[x] signal: Error while installing SIGABRT signal handler.\n");
+    return 1;
+  }
+
+  if (signal(SIGTERM, run_stop) == SIG_ERR) {
+    fprintf(stderr, "[x] signal: Error while installing SIGTERM signal handler.\n");
+    return 1;
+  }
+
   struct uvr_ws_core uvr_ws_core;
-  struct uvr_ws_destroy ws;
   memset(&uvr_ws_core, 0, sizeof(uvr_ws_core));
   memset(&ws, 0, sizeof(ws));
 
@@ -14,11 +41,11 @@ int main(void)
 
   uvr_ws_core = uvr_ws_core_create(&wsCoreCreateInfo);
   if (!uvr_ws_core.wlDisplay || !uvr_ws_core.wlrBackend)
-    goto exit_ws;
+  { raise(SIGTERM); return 1; }
 
-exit_ws:
   ws.uvr_ws_core = uvr_ws_core;
-  uvr_ws_destroy(&ws);
+
+  wl_display_run(uvr_ws_core.wlDisplay);
 
   return 0;
 }
