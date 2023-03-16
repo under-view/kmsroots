514802515c50d6b0ba14936f4a298f384c9415ee
Vincent Davis Jr <vince@underview.tech>
2023-03-16 00:30:06 -0500

    gltf-loader: add new function buffer_view_indices_acquire
    Function call acquires the byteOffset and byteSize
    of each vertex index buffer contained in larger
    buffer.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- include/gltf-loader.h
+++ include/gltf-loader.h
@@ -44,15 +44,65 @@ struct uvr_gltf_loader_file uvr_gltf_loader_file_load(struct uvr_gltf_loader_fil
 
 
 /*
+ * struct uvr_gltf_loader_indices_info (Underview Renderer GLTF Loader Indices Info)
+ *
+ * members:
+ * @indices    - The index of the accessor that links the bufferView with the vertex indices.
+ * @byteOffset - The byte offset of where the vertex indices are located within the larger buffer.
+ * @bufferSize - The size in bytes of the vertex indices buffer
+ */
+struct uvr_gltf_loader_indices_info {
+  int indices;
+  int byteOffset;
+  int bufferSize;
+};
+
+
+/*
+ * struct uvr_gltf_loader_indices (Underview Renderer GLTF Loader Indices)
+ *
+ * members:
+ * @indicesInfo      - Pointer to an array of information about a given mesh->primitive->indices.
+ *                     The "indices" index being the accessors array element associated with a
+ *                     given buffer view. The buffer view then contains the index buffer
+ *                     byte offset and buffer size.
+ * @indicesInfoCount - Amount of elements in @indicesInfo array
+ */
+struct uvr_gltf_loader_indices {
+  struct uvr_gltf_loader_indices_info *indicesInfo;
+  uint32_t                            indicesInfoCount;
+};
+
+
+/*
+ * uvr_gltf_loader_buffer_view_indices_acquire: Function acquires location of index buffer data contained in larger
+ *                                              buffer. @indicesInfo member is a pointer to an array of buffer
+ *                                              offset's and buffer byte sizes for the vertex index buffers.
+ *
+ *
+ * args:
+ * @uvrgltf - Must pass a pointer to a struct uvr_gltf_loader_file
+ * return:
+ *    on success struct uvr_gltf_loader_indices { with member being pointer to an array }
+ *    on failure struct uvr_gltf_loader_indices { with member nulled }
+ */
+struct uvr_gltf_loader_indices uvr_gltf_loader_buffer_view_indices_acquire(struct uvr_gltf_loader_file *uvrgltf);
+
+
+/*
  * struct uvr_gltf_loader_destroy (Underview Renderer Shader Destroy)
  *
  * members:
- * @uvr_gltf_loader_file_cnt - Must pass the amount of elements in struct uvr_gltf_loader_file array
- * @uvr_gltf_loader_file     - Must pass a pointer to an array of valid struct uvr_gltf_loader_file { free'd  members: cgltf_data *gltfData }
+ * @uvr_gltf_loader_file_cnt    - Must pass the amount of elements in struct uvr_gltf_loader_file array
+ * @uvr_gltf_loader_file        - Must pass a pointer to an array of valid struct uvr_gltf_loader_file { free'd  members: cgltf_data *gltfData }
+ * @uvr_gltf_loader_indices_cnt - Must pass the amount of elements in struct uvr_gltf_loader_indices_cnt array
+ * @uvr_gltf_loader_indices     - Must pass a pointer to an array of valid struct uvr_gltf_loader_indices { free'd  members: *indicesInfo }
  */
 struct uvr_gltf_loader_destroy {
-  uint32_t                     uvr_gltf_loader_file_cnt;
-  struct uvr_gltf_loader_file  *uvr_gltf_loader_file;
+  uint32_t                       uvr_gltf_loader_file_cnt;
+  struct uvr_gltf_loader_file    *uvr_gltf_loader_file;
+  uint32_t                       uvr_gltf_loader_indices_cnt;
+  struct uvr_gltf_loader_indices *uvr_gltf_loader_indices;
 };
 
 
--- src/gltf-loader.c
+++ src/gltf-loader.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "gltf-loader.h"
 
 
@@ -37,11 +40,52 @@ exit_error_uvr_gltf_loader_file_load:
 }
 
 
+struct uvr_gltf_loader_indices uvr_gltf_loader_buffer_view_indices_acquire(struct uvr_gltf_loader_file *uvrgltf)
+{
+  cgltf_size i, j, indicesInfoCount = 0;
+  cgltf_buffer_view *bufferView = NULL;
+  cgltf_data *gltfData = uvrgltf->gltfData;
+
+  struct uvr_gltf_loader_indices_info *indicesInfo = NULL;
+
+  for (i = 0; i < gltfData->meshes_count; i++)
+    for (j = 0; j < gltfData->meshes[i].primitives_count; j++)
+      indicesInfoCount++;
+
+  indicesInfo = calloc(indicesInfoCount, sizeof(struct uvr_gltf_loader_indices_info));
+  if (!indicesInfo) {
+    uvr_utils_log(UVR_DANGER, "[x] calloc: %s", strerror(errno));
+    goto exit_error_uvr_gltf_loader_buffer_view_indices_acquire;
+  }
+
+  indicesInfoCount=0;
+  for (i = 0; i < gltfData->meshes_count; i++) {
+    for (j = 0; j < gltfData->meshes[i].primitives_count; j++) {
+      if (!gltfData->meshes[i].primitives[j].indices) continue;
+      bufferView = gltfData->meshes[i].primitives[j].indices->buffer_view;
+      indicesInfo[indicesInfoCount].indices = gltfData->meshes[i].primitives[j].indices_index;
+      indicesInfo[indicesInfoCount].byteOffset = bufferView->offset;
+      indicesInfo[indicesInfoCount].bufferSize = bufferView->size;
+      indicesInfoCount++;
+    }
+  }
+
+  return (struct uvr_gltf_loader_indices) { .indicesInfo = indicesInfo, .indicesInfoCount = indicesInfoCount };
+
+exit_error_uvr_gltf_loader_buffer_view_indices_acquire:
+  return (struct uvr_gltf_loader_indices) { .indicesInfo = NULL, .indicesInfoCount = 0 };
+}
+
+
 void uvr_gltf_loader_destroy(struct uvr_gltf_loader_destroy *uvrgltf)
 {
   uint32_t i;
   for (i = 0; i < uvrgltf->uvr_gltf_loader_file_cnt; i++) {
     if (uvrgltf->uvr_gltf_loader_file[i].gltfData)
       cgltf_free(uvrgltf->uvr_gltf_loader_file[i].gltfData);
+  }
+  for (i = 0; i < uvrgltf->uvr_gltf_loader_indices_cnt; i++) {
+    if (uvrgltf->uvr_gltf_loader_indices[i].indicesInfo)
+      free(uvrgltf->uvr_gltf_loader_indices[i].indicesInfo);
   }
 }
