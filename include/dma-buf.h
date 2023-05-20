9e671b8de72141f2fb0b3fd38d5c5348ecb0e8ed
Vincent Davis Jr <vince@underview.tech>
2023-05-20 02:02:46 -0500

    dma-buf: add new file class kmr_dma_buf
    Allow for graphics API implementation to import/export
    synchronization fd's associated with DMA Buffers.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ include/dma-buf.h
@@ -0,0 +1,121 @@
+#ifndef KMR_DMA_BUF_H
+#define KMR_DMA_BUF_H
+
+#include "utils.h"
+
+/*
+ * similar comments may be found in wlroots
+ * https://gitlab.freedesktop.org/wlroots/wlroots/-/blob/master/include/render/dmabuf.h
+ */
+
+
+/*
+ * enum kmr_dma_buf_sync_flags (kmsroots DMA Buffer Synchronization Flags)
+ *
+ */
+typedef enum _kmr_dma_buf_sync_flags {
+	KMR_DMA_BUF_SYNC_READ  = (1 << 0),
+	KMR_DMA_BUF_SYNC_WRITE = (2 << 0),
+	KMR_DMA_BUF_SYNC_RW    = (KMR_DMA_BUF_SYNC_READ | KMR_DMA_BUF_SYNC_WRITE)
+} kmr_dma_buf_sync_flags;
+
+
+/*
+ * struct kmr_dma_buf_import_sync_file_create_info (kmsroots DMA Buffer Import Synchronization File Descriptor Create Info)
+ *
+ * members:
+ * @dmaBufferFdsCount - Array size of @dmaBufferFds
+ * @dmaBufferFds      - Pointer to an array of file descriptors to DMA-BUF's of size @dmaBufferFdsCount.
+ * @syncFileFd        - File descriptor to a graphics API synchronization primitive.
+ * @syncFlags         - Flags used to determine permission allowed after import
+ */
+struct kmr_dma_buf_import_sync_file_create_info {
+	uint8_t                dmaBufferFdsCount;
+	int                    *dmaBufferFds;
+	int                    syncFileFd;
+	kmr_dma_buf_sync_flags syncFlags;
+};
+
+
+/*
+ * kmr_dma_buf_import_sync_file_create: Import single graphics API synchronization primitive file descriptor into an array of DMA-BUF
+ *                                      file descriptors with DMA_BUF_IOCTL_IMPORT_SYNC_FILE. @syncFileFd in vulkan may be acquired via
+ *                                      (VkSemaphoreGetFdInfoKHR->vkGetSemaphoreFdKHR)
+ *
+ * args:
+ * @kmrdma - pointer to a struct kmr_dma_buf_import_sync_file_create_info
+ * return:
+ *	on success 0
+ *	on failure -1
+ */
+int kmr_dma_buf_import_sync_file_create(struct kmr_dma_buf_import_sync_file_create_info *kmrdma);
+
+
+/*
+ * struct kmr_dma_buf_export_sync_file_create_info (kmsroots DMA Buffer Export Synchronization File Descriptor Create Info)
+ *
+ * members:
+ * @dmaBufferFdsCount - Array size of @dmaBufferFds
+ * @dmaBufferFds      - Pointer to an array of file descriptors to DMA-BUF's of size @dmaBufferFdsCount.
+ * @syncFlags         - Flags used to determine permission allowed after export
+ */
+struct kmr_dma_buf_export_sync_file_create_info {
+	uint8_t                dmaBufferFdsCount;
+	int                    *dmaBufferFds;
+	kmr_dma_buf_sync_flags syncFlags;
+};
+
+
+/*
+ * struct kmr_dma_buf_export_sync_file (kmsroots DMA Buffer Export Synchronization File Descriptor)
+ *
+ * members:
+ * @syncFileFdsCount - Array size of @syncFileFds
+ * @syncFileFds      - Pointer to an array of file descriptors of size @syncFileFdsCount. These file descriptors
+ *                     can then be imported by graphics API implementation.
+ */
+struct kmr_dma_buf_export_sync_file {
+	uint8_t syncFileFdsCount;
+	int     *syncFileFds;
+};
+
+
+/*
+ * kmr_dma_buf_export_sync_file_create: Exports an array of synchronization file descriptors from an array of
+ *                                      DMA-BUF file descriptors with DMA_BUF_IOCTL_EXPORT_SYNC_FILE. @syncFileFds
+ *                                      in vulkan may be imported via (VkImportSemaphoreFdInfoKHR->vkImportSemaphoreFdKHR).
+ *
+ * args:
+ * @kmrdma - pointer to a struct kmr_dma_buf_export_sync_file_create_info
+ * return:
+ *	on success struct kmr_dma_buf_export_sync_file
+ *	on failure struct kmr_dma_buf_export_sync_file { with members nulled }
+ */
+struct kmr_dma_buf_export_sync_file kmr_dma_buf_export_sync_file_create(struct kmr_dma_buf_export_sync_file_create_info *kmrdma);
+
+
+/*
+ * struct kmr_dma_buf_destroy (kmsroots DMA Buffer Destroy)
+ *
+ * members:
+ *
+ * @kmr_dma_buf_export_sync_file_cnt - Must pass the amount of elements in struct kmr_dma_buf_export_sync_file
+ * @kmr_dma_buf_export_sync_file     - Must pass a pointer to an array of valid struct kmr_dma_buf_export_sync_file { free'd members: @syncFileFds }
+ */
+struct kmr_dma_buf_destroy {
+	uint32_t                            kmr_dma_buf_export_sync_file_cnt;
+	struct kmr_dma_buf_export_sync_file *kmr_dma_buf_export_sync_file;
+};
+
+
+/*
+ * kmr_dma_buf_destroy: frees any allocated memory defined by user
+ *
+ * args:
+ * @kmrdma - pointer to a struct kmr_vk_destroy contains all objects created during
+ *          application lifetime in need freeing
+ */
+void kmr_dma_buf_destroy(struct kmr_dma_buf_destroy *kmrdma);
+
+
+#endif
---
+++ src/dma-buf.c
@@ -0,0 +1,175 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <linux/dma-buf.h>
+#include <linux/version.h>
+#include <sys/utsname.h>
+#include <xf86drm.h>
+
+#include "dma-buf.h"
+
+
+/*
+ * Largely just a copy of what's in wlroots
+ * https://gitlab.freedesktop.org/wlroots/wlroots/-/blob/master/render/dmabuf_linux.c
+ */
+
+
+/**
+ * Check whether DMA-BUF import/export from/to sync_file is available.
+ *
+ * If this function returns true, dmabuf_import_sync_file() is supported.
+ */
+static bool dmabuf_check_sync_file_import_export(void)
+{
+	/* Unfortunately there's no better way to check the availability of the
+	 * IOCTL than to check the kernel version. See the discussion at:
+	 * https://lore.kernel.org/dri-devel/20220601161303.64797-1-contact@emersion.fr/
+	 */
+
+	struct utsname utsname = {0};
+	if (uname(&utsname) == -1) {
+		kmr_utils_log(KMR_DANGER, "[x] uname: %s", strerror(errno));
+		return false;
+	}
+
+	if (strncmp(utsname.sysname, "Linux", 8) != 0) {
+		kmr_utils_log(KMR_DANGER, "[x] strcmp: operating system name incorrect");
+		return false;
+	}
+
+	// Trim release suffix if any, e.g. "-arch1-1"
+	for (size_t i = 0; utsname.release[i] != '\0'; i++) {
+		char ch = utsname.release[i];
+		if ((ch < '0' || ch > '9') && ch != '.') {
+			utsname.release[i] = '\0';
+			break;
+		}
+	}
+
+	char *rel = strtok(utsname.release, ".");
+	int major = atoi(rel);
+
+	int minor = 0;
+	rel = strtok(NULL, ".");
+	if (rel != NULL) {
+		minor = atoi(rel);
+	}
+
+	int patch = 0;
+	rel = strtok(NULL, ".");
+	if (rel != NULL) {
+		patch = atoi(rel);
+	}
+
+	return KERNEL_VERSION(major, minor, patch) >= KERNEL_VERSION(5, 20, 0);
+}
+
+
+#if !defined(DMA_BUF_IOCTL_IMPORT_SYNC_FILE)
+
+struct dma_buf_import_sync_file {
+	__u32 flags;
+	__s32 fd;
+};
+
+#define DMA_BUF_IOCTL_IMPORT_SYNC_FILE _IOW(DMA_BUF_BASE, 3, struct dma_buf_import_sync_file)
+
+#endif
+
+
+#if !defined(DMA_BUF_IOCTL_EXPORT_SYNC_FILE)
+
+struct dma_buf_export_sync_file {
+	__u32 flags;
+	__s32 fd;
+};
+
+#define DMA_BUF_IOCTL_EXPORT_SYNC_FILE _IOWR(DMA_BUF_BASE, 2, struct dma_buf_export_sync_file)
+
+#endif
+
+
+int kmr_dma_buf_import_sync_file_create(struct kmr_dma_buf_import_sync_file_create_info *kmrdma)
+{
+	struct dma_buf_import_sync_file data;
+
+	if (dmabuf_check_sync_file_import_export()) {
+		kmr_utils_log(KMR_DANGER, "[x] kmr_dma_buf_import_sync_file_create: importing external fd used in synchronization to DMA-BUF fds not supported.");
+		return -1;
+	}
+
+	data.flags = kmrdma->syncFlags;
+	data.fd = kmrdma->syncFileFd;
+
+	for (uint8_t i = 0; i < kmrdma->dmaBufferFdsCount; i++) {
+		if (drmIoctl(kmrdma->dmaBufferFds[i], DMA_BUF_IOCTL_IMPORT_SYNC_FILE, &data) != 0) {
+			kmr_utils_log(KMR_DANGER, "[x] drmIoctl(DMA_BUF_IOCTL_IMPORT_SYNC_FILE)[dmaBufferFds[%u]]: %s", i, strerror(errno));
+			return -1;
+		}
+	}
+
+	close(kmrdma->syncFileFd);
+
+	return 0;
+}
+
+
+struct kmr_dma_buf_export_sync_file kmr_dma_buf_export_sync_file_create(struct kmr_dma_buf_export_sync_file_create_info *kmrdma)
+{
+	int *syncFileFds = NULL;
+	uint8_t syncFileFdsCount, i;
+	struct dma_buf_export_sync_file data;
+
+	if (dmabuf_check_sync_file_import_export()) {
+		kmr_utils_log(KMR_DANGER, "[x] kmr_dma_buf_export_sync_file_create: exporting fds used for synchronization from DMA-BUF fds not supported.");
+		goto exit_error_kmr_dma_buf_export_sync_file;
+	}
+
+	syncFileFdsCount = kmrdma->dmaBufferFdsCount;
+
+	syncFileFds = calloc(syncFileFdsCount, sizeof(int));
+	if (!syncFileFds) {
+		kmr_utils_log(KMR_DANGER, "[x] calloc: %s", strerror(errno));
+		goto exit_error_kmr_dma_buf_export_sync_file;
+	}
+
+	data.flags = kmrdma->syncFlags;
+	for (i = 0; i < syncFileFdsCount; i++) {
+		data.fd = -1;
+
+		if (drmIoctl(kmrdma->dmaBufferFds[i], DMA_BUF_IOCTL_EXPORT_SYNC_FILE, &data) != 0) {
+			kmr_utils_log(KMR_DANGER, "[x] drmIoctl(DMA_BUF_IOCTL_EXPORT_SYNC_FILE)[dmaBufferFds[%u]]: %s", i, strerror(errno));
+			goto exit_error_kmr_dma_buf_export_sync_file_free_syncFileFds;
+		}
+
+		syncFileFds[i] = data.fd;
+	}
+
+	return (struct kmr_dma_buf_export_sync_file) { .syncFileFds = syncFileFds, .syncFileFdsCount = syncFileFdsCount };
+
+exit_error_kmr_dma_buf_export_sync_file_free_syncFileFds:
+	for (i = 0; i < syncFileFdsCount; i++) {
+		if (syncFileFds[i])
+			close(syncFileFds[i]);
+	}
+	free(syncFileFds);
+exit_error_kmr_dma_buf_export_sync_file:
+	return (struct kmr_dma_buf_export_sync_file) { .syncFileFds = NULL, .syncFileFdsCount = 0 };
+}
+
+
+void kmr_dma_buf_destroy(struct kmr_dma_buf_destroy *kmrdma)
+{
+	uint32_t i, j;
+
+	for (i = 0; i < kmrdma->kmr_dma_buf_export_sync_file_cnt; i++) {
+		for (j = 0; j < kmrdma->kmr_dma_buf_export_sync_file[i].syncFileFdsCount; j++) {
+			if (kmrdma->kmr_dma_buf_export_sync_file[i].syncFileFds[j])
+				close(kmrdma->kmr_dma_buf_export_sync_file[i].syncFileFds[j]);
+			free(kmrdma->kmr_dma_buf_export_sync_file[i].syncFileFds);
+		}
+	}
+}
