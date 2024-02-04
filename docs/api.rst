0ae6aa8c749d288942542f3e7d5144481f7057d7
Vincent Davis Jr <vince@underview.tech>
2024-02-04 16:48:07 -0500

    docs: add shader interface
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- docs/api.rst
+++ docs/api.rst
@@ -12,4 +12,5 @@
 	input
 	pixel-format
 	session
+	shader
 	vulkan
--- include/shader.h
+++ include/shader.h
@@ -11,7 +11,7 @@
  * @result   - Pointer to an opaque handle to the results of a call to any shaderc_compile_into_*()
  *             Unfortunately we can't release/free until after the shader module is created.
  * @bytes    - Buffer that stores a spirv byte code.
- * @byteSize - Size of spirv byte code buffer.
+ * @byteSize - Byte size of spirv byte code buffer.
  */
 struct kmr_shader_spirv {
 	void                *result;
@@ -24,12 +24,14 @@ struct kmr_shader_spirv {
  * struct kmr_shader_spirv_create_info (kmsroots Shader SPIRV Create Information)
  *
  * members:
- * See: https://github.com/google/shaderc/blob/main/libshaderc/include/shaderc/shaderc.h#L478 for more information
+ * More information can be found at
+ * https://github.com/google/shaderc/blob/main/libshaderc/include/shaderc/shaderc.h
  * @kind        - Used to specify what type of shader to create SPIR-V bytes from
  *                key: VkShaderStageFlagBits, value: shaderc_shader_kind
  * @source      - Pointer to a buffer containing actual shader code
  * @filename    - Used as a tag to identify the source string
- * @entryPoint  - Used to define the function name in the GLSL source that acts as an entry point for the shader
+ * @entryPoint  - Used to define the function name in the GLSL source that acts
+ *                as an entry point for the shader
  */
 struct kmr_shader_spirv_create_info {
 	unsigned int kind;
@@ -47,7 +49,7 @@ struct kmr_shader_spirv_create_info {
  * parameters:
  * @spirvInfo - Pointer to a struct kmr_shader_spirv_create_info
  * returns:
- *	on success: struct kmr_shader_spirv
+ *	on success: pointer to struct kmr_shader_spirv
  *	on failure: NULL
  */
 struct kmr_shader_spirv *
