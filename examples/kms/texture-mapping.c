108c1c4864dddf4aeb36f5b28bad70328adc7b15
Vincent Davis Jr <vince@underview.tech>
2024-02-04 01:01:02 -0500

    examples[kms]: initialize inputReturnCode
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- examples/kms/texture-mapping.c
+++ examples/kms/texture-mapping.c
@@ -326,7 +326,7 @@ main (void)
 	VkExtent2D extent2D;
 	VkSurfaceFormatKHR surfaceFormat;
 
-	uint64_t inputReturnCode;
+	uint64_t inputReturnCode = 0;
 	int kmsfd = -1, inputfd = -1;
 	int nfds = -1, epollfd = -1, n;
 	enum libinput_event_type eventType;
