28a53c927bae960065b4a1d095296fdbdf39c7f8
Vincent Davis Jr <vince@underview.tech>
2025-01-04 17:16:30 -0500

    tests: switch test to utilize cmocka
    Adding in initial kernel mode
    setting test.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ tests/test-kms.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <gbm.h>
+
+/* Required by cmocka */
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include <cando/cando.h>
+
+#include "drm-node.h"
+#include "buffer.h"
+#ifdef INCLUDE_LIBSEAT
+#include "session.h"
+#endif /* INCLUDE_LIBSEAT */
+
+struct app_kms
+{
+	struct kmr_drm_node         *kmr_drm_node;
+	struct kmr_drm_node_display *kmr_drm_node_display;
+	struct kmr_buffer           *kmr_buffer;
+#ifdef INCLUDE_LIBSEAT
+	struct kmr_session          *kmr_session;
+#endif /* INCLUDE_LIBSEAT */
+};
+
+
+/********************************************
+ * Start of functions used by multiple test *
+ ********************************************/
+
+static void
+cleanup (struct app_kms *kms)
+{
+	kmr_drm_node_destroy(kms->kmr_drm_node);
+	kmr_drm_node_display_destroy(kms->kmr_drm_node_display);
+	kmr_buffer_destroy(kms->kmr_buffer);
+#ifdef INCLUDE_LIBSEAT
+	kmr_session_destroy(kms->kmr_session);
+#endif /* INCLUDE_LIBSEAT */
+}
+
+
+static int
+create_drm_context (struct app_kms *kms)
+{
+	struct kmr_drm_node_create_info kmsNodeCreateInfo;
+	struct kmr_drm_node_display_create_info displayCreateInfo;
+
+#ifdef INCLUDE_LIBSEAT
+	kms->kmr_session = kmr_session_create();
+	if (!(kms->kmr_session))
+		return -1;
+
+	kmsNodeCreateInfo.session = kms->kmr_session;
+#endif
+
+	kmsNodeCreateInfo.kmsNode = NULL;
+	kms->kmr_drm_node = kmr_drm_node_create(&kmsNodeCreateInfo);
+	if (!(kms->kmr_drm_node)) {
+		cleanup(kms);
+		return -1;
+	}
+
+	displayCreateInfo.kmsfd = kms->kmr_drm_node->kmsfd;
+	kms->kmr_drm_node_display = kmr_drm_node_display_create(&displayCreateInfo);
+	if (!(kms->kmr_drm_node_display)) {
+		cleanup(kms);
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static int
+create_gbm_buffers (struct app_kms *kms)
+{
+	struct kmr_buffer_create_info bufferCreateInfo;
+	memset(&bufferCreateInfo, 0, sizeof(bufferCreateInfo));
+
+	bufferCreateInfo.bufferType = KMR_BUFFER_GBM_BUFFER;
+	bufferCreateInfo.kmsfd = kms->kmr_drm_node_display->kmsfd;
+	bufferCreateInfo.bufferCount = 2;
+	bufferCreateInfo.width = kms->kmr_drm_node_display->width;
+	bufferCreateInfo.height = kms->kmr_drm_node_display->height;
+	bufferCreateInfo.bitDepth = 24;
+	bufferCreateInfo.bitsPerPixel = 32;
+	bufferCreateInfo.gbmBoFlags = GBM_BO_USE_SCANOUT | GBM_BO_USE_WRITE;
+	bufferCreateInfo.pixelFormat = GBM_BO_FORMAT_XRGB8888;
+	bufferCreateInfo.modifiers = NULL;
+	bufferCreateInfo.modifierCount = 0;
+
+	kms->kmr_buffer = kmr_buffer_create(&bufferCreateInfo);
+	if (!(kms->kmr_buffer)) {
+		cleanup(kms);
+		return -1;
+	}
+
+	return 0;
+}
+
+/******************************************
+ * End of functions used by multiple test *
+ ******************************************/
+
+
+/**************************************
+ * Start of test_kms_create functions *
+ **************************************/
+
+static void CANDO_UNUSED
+test_kms_create_gbm_buffer (void CANDO_UNUSED **state)
+{
+	int ret = -1;
+
+	struct app_kms app;
+	memset(&app, 0, sizeof(app));
+
+	ret = create_drm_context(&app);
+	assert_int_equal(ret, 0);
+
+	ret = create_gbm_buffers(&app);
+	assert_int_equal(ret, 0);
+
+	cleanup(&app);
+}
+
+/************************************
+ * End of test_kms_create functions *
+ ************************************/
+
+int
+main (void)
+{
+	const struct CMUnitTest tests[] =
+	{
+		cmocka_unit_test(test_kms_create_gbm_buffer),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
